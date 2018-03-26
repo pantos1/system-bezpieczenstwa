26a771592b9c655a8776aa3156faf9cfd45808fc
pantos1 <piotrek.antosiuk@gmail.com>
2018-03-26 16:33:06 +0200

    Script for ORM

---
+++ baza.py
@@ -0,0 +1,64 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Pomiary(Base):
+    __tablename__ = 'pomiary'
+
+    id_pomiaru = Column(Integer, primary_key=True)
+    id_stanu = Column(Integer, ForeignKey('stany.id_stanu', ondelete='CASCADE'), nullable=False)
+    id_odczytu = Column(Integer, ForeignKey('odczyty.id_odczytu', ondelete='CASCADE'), nullable=False)
+    id_zdjecia = Column(Integer, ForeignKey('zdjecia.id_zdjecia', ondelete='CASCADE'), nullable=False)
+
+    stany = relationship(Stany)
+    odczyt = relationship(Odczyty)
+    zdjecia = relationship(Zdjecia)
+
+class Stany(Base):
+    __tablename__ = 'stany'
+
+    id_stanu = Column(Integer, primary_key=True)
+    id_czujnika = Column(Integer, ForeignKey('czujniki.id_czujnika', ondelete='CASCADE'), nullable=False)
+
+    stan = Column(SmallInteger)
+
+    czujniki = relationship(Czujniki)
+
+class Czujniki(Base):
+    __tablename__ = 'czujniki'
+
+    id_czujnika = Column(Integer, primary_key=True)
+
+class Odczyty(Base):
+    __tablename__ = 'odczyty'
+
+    id_odczytu = Column(Integer, primary_key=True)
+    id_czujnika_temp = Column(Integer, ForeignKey('czujniki_temperatury.id_czujnika_temp', ondelete='CASCADE'), nullable=False)
+
+    temperatura = Column(Float)
+    rh = Column(Float)
+
+    czujniki_temperatury = relationship(Czujniki_temperatury)
+
+class Czujniki_temperatury(Base):
+    __tablename__ = 'czujniki_temperatury'
+
+    id_czujnika_temp = Column(Integer, primary_key=True)
+
+class Zdjecia(Base):
+    __tablename__ = 'zdjecia'
+
+    id_zdjecia = Column(Integer, primary_key=True)
+    id_kamery = Column(Integer, ForeignKey('kamery.id_kamery', ondelete='CASCADE'), nullable=False)
+
+    nazwa = Column(String)
+
+    kamery = relationship(Kamery)
+
+class Kamery(Base):
+    __tablename__ = 'kamery'
+
+    id_kamery = Column(Integer, primary_key=True)
