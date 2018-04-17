be532c54feedbc6663a7af48c2e46b92bdf2b38a
pantos1 <piotrek.antosiuk@gmail.com>
2018-04-17 16:22:27 +0200

    Fixed sql

--- Strona/get_data.php
+++ Strona/get_data.php
@@ -8,13 +8,17 @@
 		$rows = array();
         $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt  = $conn->query("SELECT *
-			FROM pomiary NATURAL JOIN zdjecia NATURAL JOIN stany NATURAL JOIN odczyty
-            WHERE pomiary.id_pomiaru =
-            (SELECT MAX(id_pomiaru) FROM pomiary)");
-		while($result = $stmt -> fetch()){
-            $rows["dane"] = $result;
-        }
+		$kamery_zapytanie = $conn->query("SELECT * FROM kamery");
+		while($kamera = $kamery_zapytanie->fetch()){
+			$stmt  = $conn->query("SELECT *
+				FROM pomiary NATURAL JOIN zdjecia NATURAL JOIN stany NATURAL JOIN odczyty
+				WHERE pomiary.id_pomiaru =
+				(SELECT MAX(id_pomiaru) FROM pomiary)
+				AND zdjecia.id_kamery = $kamera.id_kamery");
+			while($result = $stmt -> fetch()){
+				$rows[$kamera.id_kamery] = $result;
+			}
+		}
         echo json_encode($rows);
         $stmt -> closeCursor();
     }
