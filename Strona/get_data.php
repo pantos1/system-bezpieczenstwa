b681cfcd666f37e4c12fe80bdfa5521b79bfe26f
pantos1 <piotrek.antosiuk@gmail.com>
2018-04-17 14:34:10 +0200

    get_data.php fixed style

--- Strona/get_data.php
+++ Strona/get_data.php
@@ -1,28 +1,27 @@
+<?php
+	$servername = "localhost";
+    $username = "root";
+    $password = "raspberry";
+    $db = "nadzor";
+    $q = $_GET['q'];
+    try {
+		$rows = array();
+        $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt  = $conn->query("SELECT nazwa, stan, temperatura, rh
+			FROM pomiary NATURAL JOIN zdjecia NATURAL JOIN stany NATURAL JOIN odczyty
+            WHERE pomiary.id_pomiaru =
+            (SELECT MAX(id_pomiaru) FROM pomiary)");
+		while($result = $stmt -> fetch()){
+			$id_kamery = $r["id_kamery"];
+            $rows[$id] = $r;
+        }
+        echo json_encode($rows);
+        $stmt -> closeCursor();
+    }
+    catch(PDOException $e)
+    {
+		echo "Connection failed: " . $e->getMessage();
+    }
 
-          <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "raspberry";
-            $db = "nadzor";
-            $q = $_GET['q'];
-            try {
-                $rows = array();
-                $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stmt  = $conn->query("SELECT nazwa, stan, temperatura, rh
-                  FROM pomiary NATURAL JOIN zdjecia NATURAL JOIN stany NATURAL JOIN odczyty
-                  WHERE pomiary.id_pomiaru =
-                  (SELECT MAX(id_pomiaru) FROM pomiary)");
-                 while($result = $stmt -> fetch()){
-                   $id_kamery = $r["$id_kamery"];
-                   $rows[$id] = $r;
-                 }
-                 echo json_encode($rows);
-                 $stmt -> closeCursor();
-               }
-            catch(PDOException $e)
-               {
-               echo "Connection failed: " . $e->getMessage();
-               }
-
-            ?>
+?>
