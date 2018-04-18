41bf27b770497457449e334ced3f25377149c499
pantos1 <piotrek.antosiuk@gmail.com>
2018-04-18 13:36:31 +0200

    Grid to store images in archiwum

--- Strona/script/archiwum.js
+++ Strona/script/archiwum.js
@@ -5,30 +5,31 @@ $.ajax({
     success: function(result) {
 		console.log(result);
 		result = Object.values(result);
-        // for (i=0; i<result.length; i++) {
-        //     var card = $(document.createElement("div"))
-        //         .attr({
-        //             class: "uk-card uk-card-default uk-card-body"
-        //         })
-        //         .appendTo($("#grid-photo"));
-        //     $(document.createElement("h3"))
-        //         .attr({
-        //             class: "uk-card-title"
-        //         })
-        //         .text(result[i].nazwa_kamery)
-        //         .appendTo(card);
-        //     $(document.createElement("img"))
-        //         .attr({
-        //             src: "img/" + result[i].nazwa
-        //         })
-        //         .appendTo(card);
-        //     var temp = parseFloat(result[i].temperatura).toFixed(1);
-        //     var rh = parseInt(result[i].rh);
-        //     var stan = result[i].stan == '1' ? "Zamknięty" : "Otwarty";
-        //     $(document.createElement("p"))
-        //         .html("Temperatura: " + temp + "C" + "</br>Wilgotność względna: " + rh + "%" +"</br>" + result[i].opis + ": " + stan)
-        //         .appendTo(card);
-        //
-        // }
+        for (i=0; i<result.length; i++) {
+            var kameraName = $(document.createElement("div"))
+                .attr({
+                    class: "uk-h3"
+                })
+                .text(result[i]);
+            for (j = 0; j < result[i].length; j++) {
+                var card = $(document.createElement("div"))
+                    .attr({
+                        class: "uk-card uk-card-default uk-card-body"
+                    })
+                    .appendTo($("#grid-photo"));
+                $(document.createElement("img"))
+                    .attr({
+                        src: "img/" + result[i][j].nazwa
+                    })
+                    .appendTo(card);
+                var temp = parseFloat(result[i][j].temperatura).toFixed(1);
+                var rh = parseInt(result[i][j].rh);
+                var stan = result[i][j].stan === '1' ? "Zamknięty" : "Otwarty";
+                $(document.createElement("p"))
+                    .html("Temperatura: " + temp + "C" + "</br>Wilgotność względna: " + rh + "%" + "</br>" + result[i][j].opis + ": " + stan)
+                    .appendTo(card);
+
+            }
+        }
     }
 });
