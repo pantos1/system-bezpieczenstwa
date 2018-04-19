cb94325447212169f5dc712bbbb565b0a66bdf09
pantos1 <piotrek.antosiuk@gmail.com>
2018-04-19 12:49:35 +0200

    Lightbox for photos

--- Strona/script/archiwum.js
+++ Strona/script/archiwum.js
@@ -6,18 +6,35 @@ $.ajax({
 		console.log(result);
 		result = Object.values(result);
         for (i=0; i<result.length; i++) {
-            var kameraName = $(document.createElement("div"))
+            var grid = $(document.createElement("div"))
+                .id("grid-photo"+i)
+                .attr({
+                    class: "uk-padding-large uk-child-width-1-2@s uk-child-width-1-3@m uk-child-width-1-4@l uk-child-width-1-5@xl",
+                    "uk-grid": "",
+                    "uk-lightbox": "animation: slide"
+                })
+                .appendTo($("#content"));
+            $(document.createElement("div"))
                 .attr({
                     class: "uk-h3"
                 })
-                .text(result[i]);
+                .text(result[i])
+                .appendTo(grid);
 			result[i] = Object.values(result[i]);
             for (j = 0; j < result[i].length; j++) {
+                var albumElement = $(document.createElement("div"))
+                    .appendTo($("#grid-photo"));
+                var photoLink = $(document.createElement("a"))
+                    .attr({
+                        href: "img/" + result[i][j].nazwa,
+                        "data-type": "iframe"
+                    })
+                    .appendTo(albumElement);
                 var card = $(document.createElement("div"))
                     .attr({
                         class: "uk-card uk-card-default uk-card-body"
                     })
-                    .appendTo($("#grid-photo"));
+                    .appendTo(photoLink);
                 $(document.createElement("img"))
                     .attr({
                         src: "img/" + result[i][j].nazwa
