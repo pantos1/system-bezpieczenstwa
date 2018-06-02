63f2acaa2f37c532f34098d56416d58bf584e97e
pantos1 <piotrek.antosiuk@gmail.com>
2018-06-02 13:13:02 +0200

    Refactor sql variable

--- Strona/update_settings.php
+++ Strona/update_settings.php
@@ -7,12 +7,12 @@ $input = json_decode(file_get_contents('php://input'));
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "UPDATE ustawienia 
+		  SET wartosc=:wartosc
+		  WHERE klucz=:klucz";
     foreach ($input as $object) {
 		$wartosc = $object->value;
 		$klucz = $object->name;
-		$sql = "UPDATE ustawienia 
-		  SET wartosc=:wartosc
-		  WHERE :klucz";
 		$stmt = $conn->prepare($sql);
 		$stmt->execute(array(":wartosc" => $wartosc, ":klucz" => $klucz));
 		$stmt->closeCursor();
